c8e8b49b7af35d866da262d7e62f232abf3a11be
Pedro <calixtops@github.com>
2022-10-28 10:20:13 -0300

    .

--- web-search/render-build2.sh
+++ web-search/render-build2.sh
@@ -5,17 +5,16 @@ set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/web-search/ # Make sure we return to where we were
-else
-  echo "...Using Chrome from cache"
-fi
+
+echo "...Downloading Chrome"
+mkdir -p $STORAGE_DIR/chrome
+cd $STORAGE_DIR/chrome
+wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+rm ./google-chrome-stable_current_amd64.deb
+cd $HOME/ # Make sure we return to where we were
+
+
 
 pip install -r requirements.txt	
 
