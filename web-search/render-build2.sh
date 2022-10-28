49f8fa58055eaac2ce1dc61f6ba9c94a6aa2ae63
Pedro <calixtops@github.com>
2022-10-28 12:26:23 -0300

    .

--- web-search/render-build2.sh
+++ web-search/render-build2.sh
@@ -4,27 +4,27 @@ set -o errexit
 
 
 
-# STORAGE_DIR=/opt/render/project/.render
-# echo "Pasta base"
-# echo $PWD
-# echo "----------"
-# echo "...Downloading Chrome"
-mkdir -p /chrome
-# cd $STORAGE_DIR/chrome
+STORAGE_DIR=/opt/render/project/.render
+echo "Pasta base"
+echo $PWD
+echo "----------"
+echo "...Downloading Chrome"
+mkdir -p $STORAGE_DIR/chrome
+cd $STORAGE_DIR/chrome
 wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -x ./google-chrome-stable_current_amd64.deb /chrome
+dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
 rm ./google-chrome-stable_current_amd64.deb
 echo "Pasta base"
 echo $PWD
 echo "----------"
 
 
-# cd /opt/render/project/src/web-search # Make sure we return to where we were
-# echo "Pasta Final"
-# echo $PWD
-# echo $(ls)
-# echo "----------"
-pip install -r requirements.txt	
+cd /opt/render/project/src/web-search # Make sure we return to where we were
+echo "Pasta Final"
+echo $PWD
+echo $(ls)
+echo "----------"
+pip install -r /opt/render/project/src/web-search/requirements.txt	
 
 
 echo "Checando chrome folder"
