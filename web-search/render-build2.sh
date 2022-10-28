f733b598ff6efe358083b21a89c62968644aedd0
Pedro <calixtops@github.com>
2022-10-28 09:57:12 -0300

    .

--- web-search/render-build2.sh
+++ web-search/render-build2.sh
@@ -2,6 +2,21 @@
 # exit on error
 set -o errexit
 
-sudo apt install chromium-chromedriver
+
+
+STORAGE_DIR=/opt/render/project/.render
+
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/web-search/ # Make sure we return to where we were
+else
+  echo "...Using Chrome from cache"
+fi
+
 pip install -r requirements.txt	
 
