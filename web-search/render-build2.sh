96c6d8a86f1d7ab33c2e56eb9afa30fdef900a8f
Pedro <calixtops@github.com>
2022-10-28 12:22:42 -0300

    .

--- web-search/render-build2.sh
+++ web-search/render-build2.sh
@@ -4,13 +4,13 @@ set -o errexit
 
 
 
-STORAGE_DIR=/opt/render/project/.render
-echo "Pasta base"
-echo $PWD
-echo "----------"
-echo "...Downloading Chrome"
-mkdir -p $STORAGE_DIR/chrome
-cd $STORAGE_DIR/chrome
+# STORAGE_DIR=/opt/render/project/.render
+# echo "Pasta base"
+# echo $PWD
+# echo "----------"
+# echo "...Downloading Chrome"
+# mkdir -p $STORAGE_DIR/chrome
+# cd $STORAGE_DIR/chrome
 wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
 rm ./google-chrome-stable_current_amd64.deb
@@ -19,12 +19,12 @@ echo $PWD
 echo "----------"
 
 
-cd /opt/render/project/src/web-search # Make sure we return to where we were
-echo "Pasta Final"
-echo $PWD
-echo $(ls)
-echo "----------"
-pip install -r /opt/render/project/src/web-search/requirements.txt	
+# cd /opt/render/project/src/web-search # Make sure we return to where we were
+# echo "Pasta Final"
+# echo $PWD
+# echo $(ls)
+# echo "----------"
+pip install -r requirements.txt	
 
 
 echo "Checando chrome folder"
